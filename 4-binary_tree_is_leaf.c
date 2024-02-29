dea2a38845c337ba5cc7506ac6d05c295b931fc6
MohammedAmhal <mohammedamhal349@gmail.com>
2024-02-29 15:32:00 -0800

    updating the task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - this is the function that return a boolean value
+ * @tree: the binary tree
+ * Return: integer value
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL || node->right != NULL || node->left != NULL)
+	{
+		return (0);
+	}
+	return (1);
+}
