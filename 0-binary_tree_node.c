fa679fcd99900f40dcadd1013be87d7f1778ef04
MohammedAmhal <mohammedamhal349@gmail.com>
2024-02-29 11:58:21 -0800

    updating

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,25 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_t : this is the function that create a new node !
+ * binary_tree_node : this is the function that create a new node !
  * @parent: pointer that point of a tree
  * @value: the tree noeud!
- * return: a binary tree!
+ * Return: a binary tree!
 */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *tree;
-    
-    tree = malloc(sizeof(binary_tree_t));
-    if (tree == NULL)
-    {
-        return (NULL);
-    }
-    tree ->n = value;
-    tree ->parent = parent;
-    tree ->right = NULL;
-    tree ->left = NULL;
+	binary_tree_t *tree;
 
-    return (tree);
+	tree = malloc(sizeof(binary_tree_t));
+	if (tree == NULL)
+	{
+		return (NULL);
+	}
+	tree->n = value;
+	tree->parent = parent;
+	tree->right = NULL;
+	tree->left = NULL;
+
+return (tree);
 }
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,30 +1,30 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - this is the function that add the element in the left
+ * binary_tree_insert_left - this is the function return the tree
  * @parent: this is the parent of the tree
- * @value: this is the value 
+ * @value: this is the value
  * Return: the binary tree
 */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *tree = malloc(sizeof(binary_tree_t));
+	binary_tree_t *tree = malloc(sizeof(binary_tree_t));
 
-    if (tree == NULL)
-    {
-        return (NULL);
-    }
-    tree = binary_tree_node(parent, value);
-    if (tree == NULL)
-    {
-        return (NULL);
-    }
-    if (tree->left != NULL)
-    {
-        tree->left = parent->left;
-        parent->left->parent = tree;
-    }
-    parent->left = tree;
-    return (tree);
+	if (tree == NULL)
+	{
+		return (NULL);
+	}
+	tree = binary_tree_node(parent, value);
+	if (tree == NULL)
+	{
+		return (NULL);
+	}
+	if (tree->left != NULL)
+	{
+		tree->left = parent->left;
+		parent->left->parent = tree;
+	}
+	parent->left = tree;
+	return (tree);
 }
