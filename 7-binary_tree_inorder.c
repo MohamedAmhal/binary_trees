99640bea935ef1e5dba9d10ba63b280f43b1b46a
MohammedAmhal <mohammedamhal349@gmail.com>
2024-02-29 15:58:57 -0800

    updating tha task 7

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/*
+ * binary_tree_inorder - this the function that print L R R
+ * @tree : the binary tree
+ * @func: the function that print the value
+ * Return: npthing (the voif function)
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree);
+		binary_tree_inorder(tree->right, func);
+	}
+}
