51d7f310dca06e02e6a1ac88caab4adfe76ccaa5
MohammedAmhal <mohammedamhal349@gmail.com>
2024-02-29 15:53:24 -0800

    updating

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,7 +12,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree && func)
 	{
 		func(tree->n);
-		binary_tree_preorder(tree->left);
-		binary_tree_preorder(tree->right);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
 	}
 }
