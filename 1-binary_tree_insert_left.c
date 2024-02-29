00425354f7a1e7e2e0bf3a666e6519b37ae24f3e
MohammedAmhal <mohammedamhal349@gmail.com>
2024-02-29 12:14:17 -0800

    up

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	if (tree->left != NULL)
+	if (parent->left != NULL)
 	{
 		tree->left = parent->left;
 		parent->left->parent = tree;
