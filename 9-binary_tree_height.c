bdc0b99e88487ded6e60b6a8e74a9d83809a0661
MohammedAmhal <mohammedamhal349@gmail.com>
2024-02-29 16:24:25 -0800

    up

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		return 1 + binary_tree_height(tree->right) + binary_tree_height(tree->left);
+		size_t l = 0, r = 0;
+
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
 	}
 	return (0);
 }
