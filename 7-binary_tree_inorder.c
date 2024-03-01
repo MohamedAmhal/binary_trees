eea65567d7496aacfd9da8bca3535106bcaf2e59
MohammedAmhal <mohammedamhal349@gmail.com>
2024-02-29 16:00:04 -0800

    updating tha task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -12,7 +12,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree && func)
 	{
 		binary_tree_inorder(tree->left, func);
-		func(tree);
+		func(tree->n);
 		binary_tree_inorder(tree->right, func);
 	}
 }
